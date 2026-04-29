65feee240e6e55f24682c26c34c3cbf48587545c
Anupama-nambiar <annambiar@ucsd.edu>
2026-04-28 21:37:56 -0700

    audio-to-spectrogram conversion and audio waveform display (edited commented sections)

--- src/components/BoundingBoxControls.jsx
+++ src/components/BoundingBoxControls.jsx
@@ -15,26 +15,6 @@
 
 
 
-// import play from '.././assets/play.png'
-
-
-// function BoundingBoxControls() {
-//   return (
-//     <div className='p-3 bg-[#82A062] rounded-xl h-20 flex items-center gap-2'>
-//       <button className='bg-[#CAE4EF] rounded-xl w-15 h-12 flex items-center justify-center hover:bg-[#B4D2EF] cursor-pointer flex-shrink-0'>
-//         <img src={play} className='w-8 h-8' />
-//       </button>
-//       <div className='bg-white h-12 w-full rounded-lg'></div>
-//     </div>
-//   )
-// }
-
-
-// export default BoundingBoxControls
-
-
-
-
 import { useEffect, useRef, useState } from "react";
 import play from '.././assets/play.png';
 import pause from '.././assets/pause.png';
--- src/components/Spectrogram.jsx
+++ src/components/Spectrogram.jsx
@@ -10,22 +10,6 @@
 
 
 
-
-
-// function Spectrogram() {
-//     return (
-//         <div className='p-3 my-2 bg-[#82A062] rounded-xl h-100'>
-//             <div className='bg-white w-h-[100%]'>hi</div>
-//         </div>
-//     )
-// }
-
-
-// export default Spectrogram
-
-
-
-
 import { useEffect, useRef, useState, forwardRef } from "react";
 
 
