a41a7d7c69d66c764045a82d23892d019de73d26
Cailey Murad <cmmurad@ucsd.edu>
2026-05-04 16:40:52 -0700

    fixed buttons

--- src/components/BoundingBoxControls.jsx
+++ src/components/BoundingBoxControls.jsx
@@ -1,33 +1,44 @@
+import commandIcon from '../assets/command.png';
+
 function BoundingBoxControls() {
   return (
     <div className='p-2 bg-[#82A062] rounded-xl flex items-center justify-center gap-2'>
       <div className='p-2 bg-[#C8D9A3] rounded-xl flex items-center gap-1'>
-        <input placeholder='3-Char Code' className='w-25 px-2 py-1.5 text-sm bg-[#FFFFFF] rounded-md font-display placeholder-[#E6E5C9]'/>
-        <input placeholder='Species Name' className='w-50 px-2 py-1.5 text-sm bg-[#FFFFFF] rounded-md font-display placeholder-[#E6E5C9]'/>
         <button className='px-2 py-1.5 text-sm bg-[#FEECBE] rounded-md font-display whitespace-nowrap hover:bg-[#FFDE9E] cursor-pointer flex items-center gap-1'>
-          Start Labeling
-          <div className='bg-[#1E1E1E] text-white text-sm font-display px-2 rounded-md'>Enter</div>
+          Change Code
+          <div className='bg-[#1E1E1E] text-[#E6E5C9] text-sm font-display px-2 rounded-md'>Space</div>
         </button>
+        +
+        <input placeholder='3-Char Code' className='w-22 px-2 py-1.5 text-sm bg-[#FFFFFF] rounded-md font-display placeholder-[#E6E5C9]'/>
       </div>
       <div className='p-2 bg-[#C8D9A3] rounded-xl flex items-center gap-1'>
         <button className='px-2 py-1.5 text-sm bg-[#FEECBE] rounded-md font-display whitespace-nowrap hover:bg-[#FFDE9E] cursor-pointer flex items-center gap-1'>
           Select Box
-          <div className='bg-[#1E1E1E] text-white text-sm font-display px-2 rounded-md'>Tab</div>
+          <div className='bg-[#1E1E1E] text-[#E6E5C9] text-sm font-display px-2 rounded-md'>Tab</div>
         </button>
         <button className='px-2 py-1.5 text-sm bg-[#CAE4EF] rounded-md font-display whitespace-nowrap hover:bg-[#B4D2EF] cursor-pointer flex items-center gap-1'>
           Play Box Audio
-          <div className='bg-[#1E1E1E] text-white text-sm font-display px-2 rounded-md'>Shift</div>
-          <div className='bg-[#1E1E1E] text-white text-sm font-display px-2 rounded-md'>Space</div>
+          <div className='bg-[#1E1E1E] text-[#E6E5C9] text-sm font-display px-2 py-1 rounded-md'><img src={commandIcon} className='w-3 h-3'/></div>
+          <div className='bg-[#1E1E1E] text-[#E6E5C9] text-sm font-display px-2 rounded-md'>Shift</div>
         </button>
         <button className='px-2 py-1.5 text-sm bg-[#FEECBE] rounded-md font-display whitespace-nowrap hover:bg-[#FFDE9E] cursor-pointer flex items-center gap-1'>
           Delete Box
-          <div className='bg-[#1E1E1E] text-white text-sm font-display px-2 rounded-md'>Delete</div>
+          <div className='bg-[#1E1E1E] text-[#E6E5C9] text-sm font-display px-2 py-1 rounded-md'><img src={commandIcon} className='w-3 h-3'/></div>
+          <div className='bg-[#1E1E1E] text-[#E6E5C9] text-sm font-display px-2 rounded-md'>D</div>
+        </button>
+        <button className='px-2 py-1.5 text-sm bg-[#FEECBE] rounded-md font-display whitespace-nowrap hover:bg-[#FFDE9E] cursor-pointer flex items-center gap-1'>
+          Deselect Box
+          <div className='bg-[#1E1E1E] text-[#E6E5C9] text-sm font-display px-2 rounded-md'>Esc</div>
         </button>
       </div>
       <div className='p-2 bg-[#C8D9A3] rounded-xl flex items-center gap-1'>
         <button className='px-2 py-1.5 text-sm bg-[#FEECBE] rounded-md font-display whitespace-nowrap hover:bg-[#FFDE9E] cursor-pointer flex items-center gap-1'>
-          Deselect Box
-          <div className='bg-[#1E1E1E] text-white text-sm font-display px-2 rounded-md'>Esc</div>
+          Undo
+          <div className='bg-[#1E1E1E] text-[#E6E5C9] text-sm font-display px-2 rounded-md'>Z</div>
+        </button>
+        <button className='px-2 py-1.5 text-sm bg-[#FEECBE] rounded-md font-display whitespace-nowrap hover:bg-[#B4D2EF] cursor-pointer flex items-center gap-1'>
+          Redo
+          <div className='bg-[#1E1E1E] text-[#E6E5C9] text-sm font-display px-2 rounded-md'>X</div>
         </button>
       </div>
     </div>
--- src/components/SpectrogramControls.jsx
+++ src/components/SpectrogramControls.jsx
@@ -1,50 +1,48 @@
 function SpectrogramControls() {
   return (
     <div className='p-2 bg-[#82A062] rounded-xl flex items-center justify-center gap-2'>
-      <div className='p-2 bg-[#C8D9A3] rounded-xl flex items-center gap-2'>
-        <button className='px-2 py-1.5 text-sm bg-[#CAE4EF] rounded-md font-display whitespace-nowrap hover:bg-[#B4D2EF] cursor-pointer flex items-center gap-1'>
+      <button className='px-2 py-1.5 text-sm bg-[#CAE4EF] rounded-md font-display whitespace-nowrap hover:bg-[#B4D2EF] cursor-pointer flex items-center gap-1'>
           Play Audio
-          <div className='bg-[#1E1E1E] text-white text-sm font-display px-2 rounded-md'>Space</div>
-        </button>
-      </div>
+        <div className='bg-[#1E1E1E] text-[#E6E5C9] text-sm font-display px-2 rounded-md'>Shift</div>
+      </button>
       <div className='p-2 bg-[#C8D9A3] rounded-xl flex items-center gap-1'>
         <button className='px-2 py-1.5 text-sm bg-[#FEECBE] rounded-md font-display whitespace-nowrap hover:bg-[#FFDE9E] cursor-pointer flex items-center gap-1'>
           Pan Left
-          <div className='bg-[#1E1E1E] text-white text-sm font-display px-2 rounded-md'>A</div>
+          <div className='bg-[#1E1E1E] text-[#E6E5C9] text-sm font-display px-2 rounded-md'>A</div>
         </button>
         <button className='px-2 py-1.5 text-sm bg-[#FEECBE] rounded-md font-display whitespace-nowrap hover:bg-[#FFDE9E] cursor-pointer flex items-center gap-1'>
           Pan Right
-          <div className='bg-[#1E1E1E] text-white text-sm font-display px-2 rounded-md'>D</div>
+          <div className='bg-[#1E1E1E] text-[#E6E5C9] text-sm font-display px-2 rounded-md'>D</div>
         </button>
         <button className='px-2 py-1.5 text-sm bg-[#FEECBE] rounded-md font-display whitespace-nowrap hover:bg-[#FFDE9E] cursor-pointer flex items-center gap-1'>
           Pan Up
-          <div className='bg-[#1E1E1E] text-white text-sm font-display px-2 rounded-md'>W</div>
+          <div className='bg-[#1E1E1E] text-[#E6E5C9] text-sm font-display px-2 rounded-md'>W</div>
         </button>
         <button className='px-2 py-1.5 text-sm bg-[#FEECBE] rounded-md font-display whitespace-nowrap hover:bg-[#FFDE9E] cursor-pointer flex items-center gap-1'>
           Pan Down
-          <div className='bg-[#1E1E1E] text-white text-sm font-display px-2 rounded-md'>S</div>
+          <div className='bg-[#1E1E1E] text-[#E6E5C9] text-sm font-display px-2 rounded-md'>S</div>
         </button>
       </div>
       <div className='p-2 bg-[#C8D9A3] rounded-xl flex items-center gap-1'>
         <button className='px-2 py-1.5 text-sm bg-[#FEECBE] rounded-md font-display whitespace-nowrap hover:bg-[#FFDE9E] cursor-pointer flex items-center gap-1'>
           Zoom In (X)
-          <div className='bg-[#1E1E1E] text-white text-sm font-display px-2 rounded-md'>Q</div>
+          <div className='bg-[#1E1E1E] text-[#E6E5C9] text-sm font-display px-2 rounded-md'>Q</div>
         </button>
         <button className='px-2 py-1.5 text-sm bg-[#FEECBE] rounded-md font-display whitespace-nowrap hover:bg-[#FFDE9E] cursor-pointer flex items-center gap-1'>
           Zoom Out (X)
-          <div className='bg-[#1E1E1E] text-white text-sm font-display px-2 rounded-md'>E</div>
+          <div className='bg-[#1E1E1E] text-[#E6E5C9] text-sm font-display px-2 rounded-md'>E</div>
         </button>
         <button className='px-2 py-1.5 text-sm bg-[#FEECBE] rounded-md font-display whitespace-nowrap hover:bg-[#FFDE9E] cursor-pointer flex items-center gap-1'>
           Zoom In (Y)
-          <div className='bg-[#1E1E1E] text-white text-sm font-display px-2 rounded-md'>R</div>
+          <div className='bg-[#1E1E1E] text-[#E6E5C9] text-sm font-display px-2 rounded-md'>R</div>
         </button>
         <button className='px-2 py-1.5 text-sm bg-[#FEECBE] rounded-md font-display whitespace-nowrap hover:bg-[#FFDE9E] cursor-pointer flex items-center gap-1'>
           Zoom Out (Y)
-          <div className='bg-[#1E1E1E] text-white text-sm font-display px-2 rounded-md'>F</div>
+          <div className='bg-[#1E1E1E] text-[#E6E5C9] text-sm font-display px-2 rounded-md'>F</div>
         </button>
         <button className='px-2 py-1.5 text-sm bg-[#FEECBE] rounded-md font-display whitespace-nowrap hover:bg-[#FFDE9E] cursor-pointer flex items-center gap-1'>
           Reset View
-          <div className='bg-[#1E1E1E] text-white text-sm font-display px-2 rounded-md'>C</div>
+          <div className='bg-[#1E1E1E] text-[#E6E5C9] text-sm font-display px-2 rounded-md'>C</div>
         </button>
       </div>
     </div>
