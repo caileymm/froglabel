5b3770cd04cbc6d4790c2310ebf77820f7408ca1
Cailey Murad <cmmurad@ucsd.edu>
2026-05-14 15:11:03 -0700

    changed frequency range

--- src/utils/spectrogramConfig.js
+++ src/utils/spectrogramConfig.js
@@ -2,7 +2,7 @@ export const WAVEFORM_HEIGHT = 30;
 export const SPECTROGRAM_HEIGHT = 425;
 export const SCALE = 'mel';
 export const FREQUENCY_MIN = 0;
-export const FREQUENCY_MAX = 500;
+export const FREQUENCY_MAX = 20000;
 export const FFT_SAMPLES = 512;
 
-export const FREQ_LABELS = [500, 400, 300, 200, 100, 0];
+export const FREQ_LABELS = [20000, 15000, 10000, 7500, 5000, 2500, 0];
