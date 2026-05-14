455a46563875dbbf41d484aa9a3fb00315c80f47
Cailey Murad <cmmurad@ucsd.edu>
2026-05-13 22:21:45 -0700

    adding theme

---
+++ src/utils/theme.js
@@ -0,0 +1,31 @@
+export const defaultColors = {
+    background:          '#F9FAFB',
+    panels:              '#6B7280',
+    group:               '#D1D5DB',
+    buttons:             '#E5E7EB',
+    buttonsPressed:      '#D1D5DB',
+    audioButton:         '#BFDBFE',
+    audioButtonPressed:  '#93C5FD',
+    keyButtons:          '#1F2937',
+    keyText:             '#F9FAFB',
+    text:                '#1F2937',
+    cursor:              '#3B82F6',
+    progress:            '#E5E7EB',
+};
+
+export const frogThemeColors = {
+    background:          '#E6E5C9',
+    panels:              '#82A062',
+    group:               '#C8D9A3',
+    buttons:             '#FEECBE',
+    buttonsPressed:      '#FFDE9E',
+    audioButton:         '#CAE4EF',
+    audioButtonPressed:  '#B4D2EF',
+    keyButtons:          '#1E1E1E',
+    keyText:             '#E6E5C9',
+    text:                '#1E1E1E',
+    cursor:              '#CAE4EF',
+    progress:            '#F3F3E4',
+    boxes:                '#C8D9A3',
+    boxLabels:             '#C8D9A3'
+};
